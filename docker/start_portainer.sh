c2137cb5e52b5fe55ec9940eee85d822c5527377
Ben <bendotbike@gmail.com>
2020-12-03 19:46:20 -0500

    Create start_portainer.sh

---
+++ docker/start_portainer.sh
@@ -0,0 +1,4 @@
+# start_portainer.sh
+# Starts portainer
+echo starting portainer...
+sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
